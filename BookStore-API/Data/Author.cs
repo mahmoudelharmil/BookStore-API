8fa36f9094a1920e7e60652078735f7311682b38
Omnia Osama <Omnia Osama@OMNIAOSAMA-PC>
2020-05-28 00:52:59 +0200

    commit 2

---
+++ BookStore-API/Data/Author.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore_API.Data
+{
+    [Table("Authors")]
+    public partial class Author
+    {
+        
+    }
+}
---
+++ BookStore-API/Data/Book.cs
@@ -0,0 +1,6 @@
+﻿namespace BookStore_API.Data
+{
+    public class Book
+    {
+    }
+}
